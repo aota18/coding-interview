469f0e686a3ee37bfa148d30c7194193bcb59fb7
aota18 <sapphire031794@gmail.com>
2021-03-13 13:20:47 +0900

    IsUnique

--- java/isUnique.java
+++ java/isUnique.java
@@ -1,24 +1,30 @@
 // "static void main" must be defined in a public class.
+Last login: Sat Mar 13 12:11:02 on ttys000
+(base) ➜  ~ ls
+AndroidStudioProjects Fonts                 Public
+Applications          Library               Untitled 2.key
+Desktop               Movies                Untitled.key
+// "static void main" must be defined in a public class.
 public class Main {
-    
+
     public static boolean hasAllUnique(String str){
         // " " = Empty
         if(str.length()==0) return true;
         // "A" = Single
         if(str.length()==1) return true;
         // "A..." = More than two
-        
+
         for(int i=0; i<str.length(); i++){
            for(int j=0; j<str.length(); j++){
                if(i!=j && str.charAt(i)== str.charAt(j)) return false;
            }
         }
-        
+
         return true;
 
     }
 
-    
+
     public static void main(String[] args) {
         System.out.println(hasAllUnique("BQWERTYU"));
     }
@@ -28,3 +34,13 @@ public class Main {
 // What if you cannot use additional data structures.
 
 
+~
+~
+~
+~
+~
+~
+~
+~
+~
+"isUnique.java" 30L, 774C
